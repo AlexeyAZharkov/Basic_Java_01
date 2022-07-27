b70cc39dab7bccc97bb434667be1910db03b2832
AlexeyAZharkov <azharkov@mail.ru>
2022-07-27 06:51:12 +0300

    27-07

--- src/kata/k_5_4_7_st_54_8_2.java
+++ src/kata/k_5_4_7_st_54_8_2.java
@@ -1,11 +1,8 @@
 package kata;
 
 import java.io.*;
-import java.lang.instrument.IllegalClassFormatException;
-import java.util.Objects;
-import java.util.Scanner;
 
-public class k_5_4_7_st_54_8 {
+public class k_5_4_7_st_54_8_2 {
     public static void main(String[] args) throws IOException {
         Animal[] animalM1 = { new Animal("Cat"), new Animal("Dog"), new Animal("Elephant"),
                 new Animal("Cock"), new Animal("Bull"), new Animal("Ant"),
@@ -13,7 +10,7 @@ public class k_5_4_7_st_54_8 {
         ByteArrayOutputStream bai = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(bai);
 //        oos.writeInt(animalM1.length);
-        oos.writeInt(-1);
+        oos.writeInt(31);
         for (int i = 0; i < animalM1.length ; i++) {
             oos.writeObject(animalM1[i]);
         }
@@ -26,6 +23,7 @@ public class k_5_4_7_st_54_8 {
 
     public static Animal[] deserializeAnimalArray(byte[] data) {
         ObjectInputStream objectInputStream = null;
+        Animal[] animals;
         try {
             objectInputStream = new ObjectInputStream(new ByteArrayInputStream(data));
         } catch (IOException | NegativeArraySizeException | ClassCastException  e) {
@@ -37,20 +35,18 @@ public class k_5_4_7_st_54_8 {
         } catch (IOException | NegativeArraySizeException | ClassCastException e) {
             throw new IllegalArgumentException(e);
         }
-        Animal[] animals = new Animal[len];
-        for (int i = 0; i < len; ++i) {
-            try {
+        try {
+            animals = new Animal[len];
+            for (int i = 0; i < len; ++i) {
                 animals[i] = (Animal) objectInputStream.readObject();
-//            } catch (Exception e) {
-//                System.out.println(e.fillInStackTrace());
-//                throw new IllegalArgumentException(e);
-            } catch ( IOException | ClassNotFoundException | NegativeArraySizeException | ClassCastException e) {
-                throw new IllegalArgumentException(e);
             }
+        } catch ( IOException | ClassNotFoundException | NegativeArraySizeException | ClassCastException e) {
+            throw new IllegalArgumentException(e);
+        }
 //            catch (IOException e) {
 //                throw new IllegalArgumentException(e);
 //            }
-        }
+
         try {
             objectInputStream.close();
         } catch (IOException | NegativeArraySizeException | ClassCastException  e) {
@@ -59,22 +55,6 @@ public class k_5_4_7_st_54_8 {
         return animals;
 //        objectInputStream.close();
 
-    }
-}
-
-class Animal implements Serializable {
-    private final String name;
-
-    public Animal(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Animal) {
-            return Objects.equals(name, ((Animal) obj).name);
-        }
-        return false;
     }
 }
 
